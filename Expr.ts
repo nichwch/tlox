4d95dbc0399b877c1561e3138f437d5383c9d355
nichwch <nichwch@gmail.com>
2023-08-28 19:01:01 -0400

    working codegen!!!! finally

--- Expr.ts
+++ Expr.ts
@@ -1,1 +1,9 @@
-abstract class Expr + {}
+import { Token } from "./Token.ts";abstract class Expr { static Binary = class extends Expr { const left; const operator; const right;constructor( left:Expr, operator:Token, right:Expr) {super();this.left = left;this.operator = operator;this.right = right;}
+};
+ static Grouping = class extends Expr { const expression;constructor( expression:Expr) {super();this.expression = expression;}
+};
+ static Literal = class extends Expr { const value;constructor( value:any) {super();this.value = value;}
+};
+ static Unary = class extends Expr { const operator; const right;constructor( operator:Token, right:Expr) {super();this.operator = operator;this.right = right;}
+};
+}
--- GenerateAst.ts
+++ GenerateAst.ts
@@ -4,9 +4,16 @@ const defineAst = async (
   types: string[]
 ) => {
   const path = outputDir + "/" + baseName + ".ts";
+  await Deno.writeTextFile(path, "");
+  await Deno.writeTextFile(path, `import { Token } from "./Token.ts";`);
   await Deno.writeTextFile(path, `abstract class ${baseName} {`, {
     append: true,
   });
+  for (const type of types) {
+    const className = type.split("$")[0].trim();
+    const fields = type.split("$")[1].trim();
+    await defineType(path, baseName, className, fields);
+  }
   await Deno.writeTextFile(path, ``, { append: true });
   await Deno.writeTextFile(path, ``, { append: true });
   await Deno.writeTextFile(path, ``, { append: true });
@@ -14,14 +21,45 @@ const defineAst = async (
   await Deno.writeTextFile(path, `}`, { append: true });
 };
 
+const defineType = async (
+  path: string,
+  baseName: string,
+  className: string,
+  fieldList: string
+) => {
+  await Deno.writeTextFile(
+    path,
+    ` static ${className} = class extends ${baseName} {`,
+    {
+      append: true,
+    }
+  );
+  const fields = fieldList.split(",").map((field) => field.trim());
+  for (const field of fields) {
+    const name = field.split(":")[0];
+    await Deno.writeTextFile(path, ` const ${name};`, { append: true });
+  }
+  await Deno.writeTextFile(path, `constructor( ${fieldList}) {`, {
+    append: true,
+  });
+  await Deno.writeTextFile(path, `super();`, { append: true });
+  for (const field of fields) {
+    const name = field.split(":")[0];
+    await Deno.writeTextFile(path, `this.${name} = ${name};`, { append: true });
+  }
+  await Deno.writeTextFile(path, `}`, { append: true });
+  await Deno.writeTextFile(path, "\n", { append: true });
+  await Deno.writeTextFile(path, `};\n`, { append: true });
+};
 if (Deno.args.length !== 1) {
   console.log("Usage: generate_ast <output directory>");
   Deno.exit(64);
 }
+
 const outputDir = Deno.args[0];
 defineAst(outputDir, "Expr", [
-  "Binary : Expr left, Token operator, Expr right",
-  "Grouping : Expr expression",
-  "Literal : Object value",
-  "Unary : Token operator, Expr right",
+  "Binary $ left:Expr, operator:Token, right:Expr",
+  "Grouping $ expression:Expr",
+  "Literal $ value:any",
+  "Unary $ operator:Token, right:Expr",
 ]);
